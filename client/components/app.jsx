b13fcadb90a1f08709c1e287f5dbc3aaeaaa7708
Mohsinul Huq <kusholhuq@gmail.com>
2020-04-18 21:30:02 -0700

    Header component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,16 +1,20 @@
 
 import React from 'react';
+import Header from './header';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.Header = this.Header.bind(this);
     this.state = {
       grades: []
     };
   }
 
   render() {
-    return null;
+    return (
+      <Header text="Student Grade Table"/>
+    );
   }
 }
 
